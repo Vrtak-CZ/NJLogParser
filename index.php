b90a98775b1ad3734a54363f5a9c1088ef746ca9
Patrik Votocek <patrik@votocek.cz>
2012-05-03 21:15:35 +0200

    Nette 2.0 and dibi -> Nette\Database

--- index.php
+++ index.php
@@ -1,42 +1,26 @@
 <?php
 namespace NJLogParser;
 
-require_once __DIR__ . "/libs/loader.php";
+require_once __DIR__ . "/libs/nette.min.php";
+require_once __DIR__ . "/libs/Curl.php";
+require_once __DIR__ . "/libs/CurlResponse.php";
+require_once __DIR__ . "/libs/Parser.php";
 
-use Nette;
-use dibi;
+use Nette\Diagnostics\Debugger;
 
 set_time_limit(0);
-Nette\Debug::enable(Nette\Debug::DEVELOPMENT);
-Nette\Debug::timer();
-Nette\Environment::setVariable("tempDir", __DIR__ . "/temp");
-$loader = new Nette\Loaders\RobotLoader();
-$loader->addDirectory(__DIR__."/libs");
-$loader->register();
+Debugger::enable(Debugger::DEVELOPMENT, __DIR__);
+Debugger::timer();
 
 /************************************ SQLite *************************************/
-/*if (file_exists(__DIR__."/db.sqlite"))
+/*if (file_exists(__DIR__."/db.sqlite")) {
 	copy(__DIR__."/db.sqlite", __DIR__."/backup/".date("Y-m-d_H-i-s-").substr(microtime(TRUE)-time(), 2, 4).".sqlite");
-dibi::connect(array(
-	'driver' => "sqlite3",
-	'database' => __DIR__."/db.sqlite",
-	'formatDateTime' => "'Y-m-d H:i:s'",
-	'formatDate' => "'Y-m-d'",
-	'lazy' => TRUE,
-	'profiler' => TRUE
-));
-dibi::loadFile(__DIR__."/db.structure.sqlite.sql");*/
+}
+$connection = new \Nette\Database\Connection('sqlite:' . __DIR__ . '/db.sqlite');
+\Nette\Database\Helpers::loadFromFile($connection, __DIR__ . '/db.structure.sqlite.sql');
 /************************************ MySQL *************************************/
-dibi::connect(array(
-	'driver' => "mysql",
-	'host' => "localhost",
-	'database' => "nettejabber",
-	'username' => "nettejabber",
-	'password' => "nettejabber",
-	'lazy' => TRUE,
-	'profiler' => TRUE
-));
-dibi::loadFile(__DIR__."/db.structure.mysql.sql");
+$connection = new \Nette\Database\Connection('mysql:host=localhost;dbname=njlogparser', 'nette', 'nette');
+\Nette\Database\Helpers::loadFromFile($connection, __DIR__ . '/db.structure.mysql.sql');
 
 if (!defined('STDIN'))
 {
@@ -44,16 +28,22 @@ if (!defined('STDIN'))
 		.'<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>Nette Jabber Log Parser</title></head><body><code><pre>';
 }
 
-Parser::$originalDataDir = __DIR__."/data";
-//Parser::$debug = TRUE;
+$parser = new Parser($connection);
 
-if (($date = Nette\Environment::getHttpRequest()->getQuery('date', NULL)) || ($date = isset($argv[1])  ? $argv[1] : NULL))
-	Parser::parseDate($date, TRUE);
+$parser->originalDataDir = __DIR__."/data";
+//$parser->debug = TRUE;
+
+$factory = new \Nette\Http\RequestFactory;
+$factory->setEncoding('UTF-8');
+$req = $factory->createHttpRequest();
+
+if (($date = $req->getQuery('date', NULL)) || ($date = isset($argv[1])  ? $argv[1] : NULL))
+	$parser->parseDate($date, TRUE);
 else
-	Parser::parse(10);
+	$parser->parse(10);
 echo "\n\n";
 
-$sec = $time = Nette\Debug::timer();
+$sec = $time = Debugger::timer();
 $hours = (int)($sec/3600);
 $sec -= $hours*3600;
 $mins = (int)($sec/60);
@@ -62,4 +52,3 @@ $sec = (int)$sec;
 echo "Total Time: ".round($time*1000, 2)."ms ".str_pad($hours, 2, 0, STR_PAD_LEFT).":".str_pad($mins, 2, 0, STR_PAD_LEFT)
 	.":".str_pad($sec, 2, 0, STR_PAD_LEFT).".".round(($time-$sec)*1000)."\n";
 echo "Memory: ".round(memory_get_usage()/1024, 2)."KB (Real Memory: ".round(memory_get_usage(TRUE)/1024, 2)."KB)\n";
-echo "SQL Queries: ".dibi::$numOfQueries." SQL Time: ".round(dibi::$totalTime*1000, 2)."ms\n\n\n\r";
--- libs/Parser.php
+++ libs/Parser.php
@@ -2,7 +2,7 @@
 /**
  * Nette Jabber Log Parser
  *
- * @copyright  Copyright (c) 2010 Patrik Votoček
+ * @copyright  Copyright (c) 2012 Patrik Votoček
  * @license    http://nellacms.com/license  New BSD License
  * @link       http://addons.nettephp.com/cs/active-mapper
  * @category   NJLogParser
@@ -11,33 +11,42 @@
 
 namespace NJLogParser;
 
-use dibi;
-use DateTime;
-use DateInterval;
+use DateTime, DateInterval;
 
 /**
  * Parser
  *
  * @author     Patrik Votoček
- * @copyright  Copyright (c) 2010 Patrik Votoček
+ * @copyright  Copyright (c) 2012 Patrik Votoček
  * @package    NJLogParser
  */
 class Parser
 {
-	const VERSION = "1.2";
+	const VERSION = "1.3";
+
+	/** @var \Nette\Database\Connection */
+	private $connection;
 
 	/** @var string */
-	public static $originalDataDir;
+	public $originalDataDir;
 	/** @var string */
-	public static $startDate = "2009-05-12";
+	public $startDate = "2009-05-12";
 	/** @var bool */
-	public static $debug = FALSE;
+	public $debug = FALSE;
 	/** @var int */
-	private static $totalDays = NULL;
+	private $totalDays = NULL;
 	/** @var int */
-	private static $parsedDays = 0;
+	private $parsedDays = 0;
 	/** @var int */
-	private static $progressTime = NULL;
+	private $progressTime = NULL;
+
+	/**
+	 * @param \Nette\Database\Connection
+	 */
+	public function __construct(\Nette\Database\Connection $connection)
+	{
+		$this->connection = $connection;
+	}
 
 
 	/**
@@ -45,41 +54,42 @@ class Parser
 	 *
 	 * @param int $dayLimit parse day limit
 	 */
-	public static function parse($dayLimit = NULL)
+	public function parse($dayLimit = NULL)
 	{
-		$date = new DateTime(self::$startDate);
+		$date = new DateTime($this->startDate);
 		if (empty($dayLimit))
-			self::$totalDays = (int)((time()-$date->getTimestamp())/86400)+1;
+			$this->totalDays = (int)((time()-$date->getTimestamp())/86400)+1;
 		else
-			self::$totalDays = $dayLimit;
+			$this->totalDays = $dayLimit;
 		
 		$myDate = new DateTime();
 		$parsetime = $myDate->sub(new DateInterval("P2D"))->getTimestamp();
 		unset($myDate);
 		
-		self::$parsedDays = 1;
+		$this->parsedDays = 1;
 		do {
-			if (($dayLimit !== NULL && self::$totalDays < self::$parsedDays) || $date->getTimestamp() > time())
+			if (($dayLimit !== NULL && $this->totalDays < $this->parsedDays) || $date->getTimestamp() > time()) {
 				break;
-			if (!defined('STDIN') && !self::$debug)//Next code is realy FUCKING hack
-			{
+			}
+			if (!defined('STDIN') && !$this->debug) { //Next code is realy FUCKING hack
 				echo $date->format("Y-m-d") . "<br>\n";
 				@ob_end_flush(); 
-		    @ob_flush(); 
-		    @flush(); 
-		    @ob_start();
-		  }
+			    @ob_flush(); 
+			    @flush(); 
+			    @ob_start();
+		  	}
 
-			dibi::begin();
-			if (dibi::select('id')->from('parsed')->where("[date] = %d", $date->format("Y-m-d"))->fetchSingle() === FALSE || $date->getTimestamp() > $parsetime)
-			{
-				self::parseDate($date->format("Y-m-d"));
-				dibi::insert('parsed', array('date' => $date->format("Y-m-d")))->execute();
-				self::$parsedDays++;
+			$this->connection->beginTransaction();
+			$selection = $this->connection->table('parsed');
+			if ($selection->where("date = ?", $date->format("Y-m-d"))->count('*') < 1 || $date->getTimestamp() > $parsetime) {
+				$this->parseDate($date->format("Y-m-d"));
+				$selection->insert(array('date' => $date->format("Y-m-d")));
+				$this->parsedDays++;
+			} elseif ($dayLimit === NULL) {
+				$this->parsedDays++;
 			}
-			elseif ($dayLimit === NULL)
-				self::$parsedDays++;
-			dibi::commit();
+			
+			$this->connection->commit();
 		} while ($date->add(new DateInterval('P1D'))->getTimestamp() < time());
 	}
 
@@ -89,13 +99,14 @@ class Parser
 	 * @param string $path
 	 * @return array
 	 */
-	public static function parseDate($date, $reload = FALSE)
+	public function parseDate($date, $reload = FALSE)
 	{
-		self::log("parse-start", $date);
-		$data = self::loadData($date, $reload);
-		if (!empty($data))
-			self::parseData ($data, $date);
-		self::log("parse-end", $date);
+		$this->log("parse-start", $date);
+		$data = $this->loadData($date, $reload);
+		if (!empty($data)) {
+			$this->parseData($data, $date);
+		}
+		$this->log("parse-end", $date);
 	}
 
 	/**
@@ -105,17 +116,16 @@ class Parser
 	 * @param bool $reload
 	 * @return string
 	 */
-	private static function loadData($date, $reload = FALSE)
+	private function loadData($date, $reload = FALSE)
 	{
-		self::log("load-data-start", $date);
+		$this->log("load-data-start", $date);
 		$parseDate = new DateTime($date);
 		$myDate = new DateTime();
 		$myDate->sub(new DateInterval("P2D"));
 		$data = NULL;
-		if ($reload || !file_exists(self::$originalDataDir."/".$date.".html") || $parseDate->getTimestamp() > $myDate->getTimestamp())
-		{
+		if ($reload || !file_exists($this->originalDataDir."/".$date.".html") || $parseDate->getTimestamp() > $myDate->getTimestamp()) {
 			$curl = new \Curl();
-			$curl->setUserAgent("Mozilla/5.0 (compatible; NetteJabberLogParser/".self::VERSION."; +http://nettejabber.jdem.cz)");
+			$curl->setUserAgent("Mozilla/5.0 (compatible; NetteJabberLogParser/".static::VERSION."; +http://nettejabber.jdem.cz)");
 			$curl->setHeader("HTTP_ACCEPT", "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
 			$curl->setHeader("HTTP_ACCEPT_LANGUAGE", "cs-cz,cs,en-us;q=0.7,en;q=0.3");
 			$curl->setHeader("HTTP_ACCEPT_ENCODING", "gzip,deflate");
@@ -123,16 +133,16 @@ class Parser
 			$curl->setHeader("HTTP_KEEP_ALIVE", 300);
 			$curl->setHeader("HTTP_CONNECTION", "keep-alive");
 			$response = $curl->get("http://nezmar.jabbim.cz/logs/nette@conf.netlab.cz/".str_replace("-", "/", $date).".html");
-			if ($response->getHeader("Status-Code") == 200)
-			{
-				file_put_contents(self::$originalDataDir."/".$date.".html", $response->getBody());
+			if ($response->getHeader("Status-Code") == 200) {
+				file_put_contents($this->originalDataDir."/".$date.".html", $response->getBody());
 				$data = $response->getBody();
 			}
 		}
-		elseif (file_exists(self::$originalDataDir."/".$date.".html"))
-				$data = file_get_contents(self::$originalDataDir."/".$date.".html");
+		elseif (file_exists($this->originalDataDir."/".$date.".html")) {
+			$data = file_get_contents($this->originalDataDir."/".$date.".html");
+		}
 
-		self::log("load-data-end", $date);
+		$this->log("load-data-end", $date);
 		return $data;
 	}
 
@@ -142,38 +152,39 @@ class Parser
 	 * @param string $data
 	 * @return array
 	 */
-	private static function parseData($data, $date)
+	private function parseData($data, $date)
 	{
-		self::log("parse-data-start", $date);
+		$this->log("parse-data-start", $date);
 		$data = substr($data, strpos($data, '<a id="'));
 		$data = substr($data, 0, strpos($data, '<div class="legend">') - 1);
 		$data = explode("<br/>\n<a id", $data);
 
 		$i = 1;
-		foreach ($data as $line)
-		{
-			if (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mj\"\>(.+) joins the room.+/i", $line, $matches) === 1)
-				self::add($date." ".$matches[1], $matches[2], "join", $matches[3]);
-			elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"ml\"\>(.+) leaves the room.+/i", $line, $matches) === 1)
-				self::add($date." ".$matches[1], $matches[2], "leave", $matches[3]);
-			elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mn\"\>&lt;(.+)&gt;\<\/font\> (.*)/i", $line, $matches) === 1)
-				self::add($date." ".$matches[1], $matches[2], "message", $matches[3], $matches[4]);
-			elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"msc\"\>(.+) has set the subject to: (.*)\<\/font\>/i", $line, $matches) === 1)
-				self::add($date." ".$matches[1], $matches[2], "subject", $matches[3], $matches[4]);
-			elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mnc\"\>(.+) is now known as (.*)\<\/font\>/i", $line, $matches) === 1)
-				self::add($date." ".$matches[1], $matches[2], "rename", $matches[3], $matches[4]);
-			elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+class=\"ts\"\>(.+) has been kicked: (.*)/i", $line, $matches) === 1)
-				self::add($date." ".$matches[1], $matches[2], "kick", $matches[3], $matches[4]);
-			elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mne\"\>([^ ]+) (.*)\<\/font\>/i", $line, $matches) === 1)
-				self::add($date." ".$matches[1], $matches[2], "status", $matches[3], $matches[4]);
-			else
-				self::log('unknow', $line);
+		foreach ($data as $line) {
+			if (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mj\"\>(.+) joins the room.+/i", $line, $matches) === 1) {
+				$this->add($date." ".$matches[1], $matches[2], "join", $matches[3]);
+			} elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"ml\"\>(.+) leaves the room.+/i", $line, $matches) === 1) {
+				$this->add($date." ".$matches[1], $matches[2], "leave", $matches[3]);
+			} elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mn\"\>&lt;(.+)&gt;\<\/font\> (.*)/i", $line, $matches) === 1) {
+				$this->add($date." ".$matches[1], $matches[2], "message", $matches[3], $matches[4]);
+			} elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"msc\"\>(.+) has set the subject to: (.*)\<\/font\>/i", $line, $matches) === 1) {
+				$this->add($date." ".$matches[1], $matches[2], "subject", $matches[3], $matches[4]);
+			} elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mnc\"\>(.+) is now known as (.*)\<\/font\>/i", $line, $matches) === 1) {
+				$this->add($date." ".$matches[1], $matches[2], "rename", $matches[3], $matches[4]);
+			} elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+class=\"ts\"\>(.+) has been kicked: (.*)/i", $line, $matches) === 1) {
+				$this->add($date." ".$matches[1], $matches[2], "kick", $matches[3], $matches[4]);
+			} elseif (preg_match("/.+name=\"([0-9:]+)\.([0-9]+)\".+\<font class=\"mne\"\>([^ ]+) (.*)\<\/font\>/i", $line, $matches) === 1) {
+				$this->add($date." ".$matches[1], $matches[2], "status", $matches[3], $matches[4]);
+			} else {
+				$this->log('unknow', $line);
+			}
 
-			if (defined('STDIN') && !self::$debug)
-				self::progressBar($i, count($data));
+			if (defined('STDIN') && !$this->debug) {
+				$this->progressBar($i, count($data));
+			}
 			$i++;
 		}
-		self::log("parse-data-end", $date);
+		$this->log("parse-data-end", $date);
 	}
 
 	/**
@@ -185,17 +196,25 @@ class Parser
 	 * @param string $name
 	 * @param string $data
 	 */
-	private static function add($datetime, $ms, $type, $name, $data = NULL)
+	private function add($datetime, $ms, $type, $name, $data = NULL)
 	{
-		if (dibi::select('id')->from('data')->where("[datetime] = %t AND [ms] = %i", $datetime, $ms)->fetchSingle() !== FALSE)
-		{
-			dibi::update('data', array('type' => $type, 'name' => $name, 'data' => $data))->where("[datetime] = %d AND [ms] = %i", $datetime, $ms)->execute();
-			self::log("update", $datetime . "#" . $type);
-		}
-		else
-		{
-			dibi::insert('data', array('datetime' => $datetime, 'ms' => $ms, 'type' => $type, 'name' => $name, 'data' => $data))->execute();
-			self::log("add", $datetime . "#" . $type);
+		$selection = $this->connection->table('data');
+		if ($selection->where("datetime = ? AND ms = ?", $datetime, $ms)->count('*') > 0) {
+			$selection->update(array(
+				'type' => $type, 
+				'name' => $name, 
+				'data' => $data
+			))->where("datetime = ? AND ms = ?", $datetime, $ms);
+			$this->log("update", $datetime . "#" . $type);
+		} else {
+			$selection->insert(array(
+				'datetime' => $datetime, 
+				'ms' => $ms, 
+				'type' => $type, 
+				'name' => $name, 
+				'data' => $data)
+			);
+			$this->log("add", $datetime . "#" . $type);
 		}
 	}
 
@@ -205,19 +224,22 @@ class Parser
 	 * @param string $type
 	 * @param string $message
 	 */
-	private static function log($type, $message = NULL)
+	private function log($type, $message = NULL)
 	{
-		dibi::insert("logs", array('datetime' => date("Y-m-d H:i:s"), 'type' => $type, 'message' => $message))->execute();
-		if (self::$debug)
-		{
+		$selection = $this->connection->table('logs');
+		$selection->insert(array(
+			'datetime' => date("Y-m-d H:i:s"), 
+			'type' => $type, 
+			'message' => $message,
+		));
+		if ($this->debug) {
 			echo date("Y-m-d H:i:s.") . substr((microtime(TRUE)-time())."", 2, 4) . " @ " . $type . " # " . $message . "\n";
-			if (!defined('STDIN')) //Next code is realy FUCKING hack
-			{
+			if (!defined('STDIN')) { //Next code is realy FUCKING hack
 				@ob_end_flush(); 
-		    @ob_flush(); 
-		    @flush(); 
-		    @ob_start();
-		  }
+				@ob_flush(); 
+				@flush(); 
+				@ob_start();
+			}
 		}
 	}
 
@@ -242,23 +264,26 @@ class Parser
 	 * @param int $size optional size of the status bar
 	 * @return void
 	 */
-	private static function progressBar($doneLines, $totalLines, $size=10)
+	private function progressBar($doneLines, $totalLines, $size=10)
 	{
-		if(self::$parsedDays > self::$totalDays)
+		if($this->parsedDays > $this->totalDays) {
 			return;
-		if(empty(self::$progressTime))
-			self::$progressTime=microtime(TRUE);
+		}
+		if(empty($this->progressTime)) {
+			$this->progressTime=microtime(TRUE);
+		}
 
-		if ($doneLines < $totalLines)
-			$perc=(double)((self::$parsedDays-1+((double)($doneLines/$totalLines)))/self::$totalDays);
-		else
-			$perc=(double)(self::$parsedDays/self::$totalDays);
+		if ($doneLines < $totalLines) {
+			$perc=(double)(($this->parsedDays-1+((double)($doneLines/$totalLines)))/$this->totalDays);
+		} else {
+			$perc=(double)($this->parsedDays/$this->totalDays);
+		}
 
 		$bar=floor($perc*$size);
 
 		$status_bar="\r[";
 		$status_bar.=str_repeat("=", $bar);
-		if($bar<$size){
+		if($bar<$size) {
 			$status_bar.=">";
 			$status_bar.=str_repeat(" ", $size-$bar);
 		} else {
@@ -267,7 +292,7 @@ class Parser
 
 		$disp=number_format($perc*100, 0);
 
-		$status_bar.="]".str_pad($disp, 3, ' ', STR_PAD_LEFT)."%  ".str_pad(self::$parsedDays, strlen(self::$totalDays.""), ' ', STR_PAD_LEFT)."/".self::$totalDays;
+		$status_bar.="]".str_pad($disp, 3, ' ', STR_PAD_LEFT)."%  ".str_pad($this->parsedDays, strlen($this->totalDays.""), ' ', STR_PAD_LEFT)."/".$this->totalDays;
 
 		/***************************** lines ******************************/
 		$perc=(double)($doneLines/$totalLines);
@@ -276,7 +301,7 @@ class Parser
 
 		$status_bar.=" [";
 		$status_bar.=str_repeat("=", $bar);
-		if($bar<$size){
+		if($bar<$size) {
 			$status_bar.=">";
 			$status_bar.=str_repeat(" ", $size-$bar);
 		} else {
@@ -288,11 +313,11 @@ class Parser
 		$status_bar.="]".str_pad($disp, 3, ' ', STR_PAD_LEFT)."% ".str_pad($doneLines, 5, 0, STR_PAD_LEFT)."/".str_pad($totalLines, 5, 0, STR_PAD_LEFT);
 		/*******************************************************************/
 
-		$rate = (microtime(TRUE)-self::$progressTime) / (self::$parsedDays + $perc);
-		$left = self::$totalDays - (self::$parsedDays - 1 + $perc);
+		$rate = (microtime(TRUE)-$this->progressTime) / ($this->parsedDays + $perc);
+		$left = $this->totalDays - ($this->parsedDays - 1 + $perc);
 		$eta = $rate * $left;
 
-		$elapsed = (int)(time() - self::$progressTime);
+		$elapsed = (int)(time() - $this->progressTime);
 
 		$status_bar.= " R:";
 		$status_bar.= ($eta > 60) ? (round($eta/60, 2)."m") : (round($eta, 2)."s");
@@ -304,7 +329,8 @@ class Parser
 		@flush();
 
 		// when done, send a newline
-		if(self::$parsedDays == self::$totalDays && $doneLines == $totalLines)
+		if($this->parsedDays == $this->totalDays && $doneLines == $totalLines) {
 			echo "\n";
+		}
 	}
 }
